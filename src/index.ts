ce23b3e71e134f6dd437ff4f6102c0eef53c447f
Sor4chi <80559385+sor4chi@users.noreply.github.com>
2024-01-27 13:53:56 +0900

    feat: add error tracer

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { Hono } from "hono";
+import type { Context } from "hono";
 import { accepts } from "hono/accept";
 import { optimizeImage } from "wasm-image-optimization";
 
@@ -11,9 +12,20 @@ const isValidUrl = (url: string) => {
 	}
 };
 
+interface OptimizeInfo {
+	width?: number;
+	quality?: number;
+	originalFormat?: string;
+	bytes?: number;
+}
+
 interface Env {
 	Bindings: {
 		DISCORD_WEBHOOK_URL: string;
+	};
+	Variables: {
+		optimizeInfo: OptimizeInfo;
+		phase: string;
 	};
 }
 
@@ -22,6 +34,7 @@ const app = new Hono<Env>();
 const FOREVER_CACHE_CONTROL = "public, max-age=31536000, immutable";
 
 app.get("*", async (c) => {
+	c.set("phase", "parse accept header");
 	const type = accepts(c, {
 		header: "Accept",
 		supports: ["image/webp", "*/*", "image/*"],
@@ -30,6 +43,7 @@ app.get("*", async (c) => {
 
 	const isWebp = type === "image/webp";
 
+	c.set("phase", "parse url from query");
 	const url = new URL(c.req.url);
 
 	const imageUrl = c.req.query("url");
@@ -37,6 +51,7 @@ app.get("*", async (c) => {
 		return c.text("valid url is required", { status: 400 });
 	}
 
+	c.set("phase", "fetch image from cache");
 	const cache = caches.default;
 	url.searchParams.append("webp", isWebp.toString());
 	const cacheKey = new Request(url.toString());
@@ -45,23 +60,31 @@ app.get("*", async (c) => {
 		return cachedResponse;
 	}
 
+	c.set("phase", "fetch image from origin");
 	const width = c.req.query("w");
+	const widthInt = width ? parseInt(width) : undefined;
 	const quality = c.req.query("q");
+	const qualityInt = quality ? parseInt(quality) : undefined;
 
-	const [srcImage, contentType] = await fetch(imageUrl, {
+	const imgRes = await fetch(imageUrl, {
 		cf: { cacheKey: imageUrl },
-	})
-		.then(async (res) =>
-			res.ok
-				? ([await res.arrayBuffer(), res.headers.get("content-type")] as const)
-				: [],
-		)
-		.catch(() => []);
-
-	if (!srcImage) {
+	});
+	if (!imgRes.ok) {
 		return c.text("image not found", { status: 404 });
 	}
 
+	c.set("phase", "parse image");
+	const srcImage = await imgRes.arrayBuffer();
+	const contentType = imgRes.headers.get("content-type") || undefined;
+
+	c.set("optimizeInfo", {
+		width: widthInt,
+		quality: qualityInt,
+		originalFormat: contentType,
+		bytes: srcImage.byteLength,
+	});
+
+	c.set("phase", "unsupported image proxy");
 	if (contentType && ["image/svg+xml", "image/gif"].includes(contentType)) {
 		const response = new Response(srcImage, {
 			headers: {
@@ -73,17 +96,23 @@ app.get("*", async (c) => {
 		return response;
 	}
 
+	c.set("phase", "optimize image");
 	const format = isWebp
 		? "webp"
 		: contentType === "image/jpeg"
 		  ? "jpeg"
 		  : "png";
+
+	c.set("phase", "optimize image, after copy");
+
 	const image = await optimizeImage({
 		image: srcImage,
-		width: width ? parseInt(width) : undefined,
-		quality: quality ? parseInt(quality) : undefined,
+		width: widthInt,
+		quality: qualityInt,
 		format,
 	});
+
+	c.set("phase", "optimized image proxy");
 	const response = new Response(image, {
 		headers: {
 			"Content-Type": `image/${format}`,
@@ -97,33 +126,46 @@ app.get("*", async (c) => {
 
 const TZ = "Asia/Tokyo";
 
-const ERROR_MESSAGE = (err: Error, url: string) =>
+const ERROR_MESSAGE = (
+	err: Error,
+	url: string,
+	phase: string,
+	info?: OptimizeInfo,
+) =>
 	`
 ## Error on ${new Date().toLocaleString("ja-JP", { timeZone: TZ })}
 
 **URL**: ${url}
 
+**Phase**: ${phase}
+
 \`\`\`
 ${err.stack}
+${JSON.stringify(info, null, 2)}
 \`\`\`
 `.trim();
 
-app.onError(async (err, c) => {
+const reportToDiscord = async (c: Context, content: string) => {
 	const webhookUrl = c.env.DISCORD_WEBHOOK_URL;
-	if (!webhookUrl)
-		return c.text("internal server error (no webhook url)", { status: 500 });
+	if (!webhookUrl) return;
 	const webhook = new URL(webhookUrl);
 	const res = await fetch(webhook.toString(), {
 		method: "POST",
 		headers: { "content-type": "application/json" },
-		body: JSON.stringify({
-			content: ERROR_MESSAGE(err, c.req.url),
-		}),
+		body: JSON.stringify({ content }),
 	});
 	if (!res.ok) {
-		return c.text("internal server error (failed to report)", { status: 500 });
+		throw new Error("failed to report error");
 	}
-	return c.text("internal server error (reported)", { status: 500 });
+};
+
+app.onError(async (err, c) => {
+	await reportToDiscord(
+		c,
+		ERROR_MESSAGE(err, c.req.url, c.get("phase"), c.get("optimizeInfo")),
+	);
+
+	return c.text("internal server error", { status: 500 });
 });
 
 export default app;
